e08104dc0e02ce4ebc93452f93ca88b69ba64453
Rizcky Ramadhan <67969694+gitrizcky@users.noreply.github.com>
2020-09-03 15:57:38 +0700

    initial commit

---
+++ src/test/java/tech/rizcky/api/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.rizcky.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
